a3566ddcd24905e965db572ed5b1246f062239c9
Anders Solberg <ssredna@gmail.com>
2020-10-30 18:33:05 +0100

    chore: add tests to see that data loads

--- cypress/integration/App.spec.tsx
+++ cypress/integration/App.spec.tsx
@@ -13,4 +13,12 @@ describe('App', () => {
         movieReducer: initMovieState,
       });
   });
+
+  it('loads 25 movies', () => {
+    cy.get('.MuiToolbar-root > .MuiTypography-root').contains('25');
+  });
+
+  it('shows the total number of films', () => {
+    cy.get('.MuiDataGrid-rowCount').contains('Total Rows: 4 799');
+  });
 });
